21964ff33f2f6cbab11fc553e6e1102fe3235b4f
burd5 <austinburdette5@gmail.com>
2023-07-31 16:18:06 -0600

    returning_strings.sql

---
+++ returning_strings.sql
@@ -0,0 +1,11 @@
+-- # Returning Strings SQL
+
+/*
+Write a select statement that takes name from person table and return "Hello, <name> how are you doing today?" results in a column named greeting
+*/
+
+
+select format('Hello, %s how are you doing today?', name) as greeting
+from person
+
+select 'Hello, ' || name || ' how are you doing today?' as greeting from person
