7e89233e07a5f2c91da93babcb58c84d0e5e0e77
burd5 <austinburdette5@gmail.com>
2023-08-04 11:37:41 -0600

    completed challenge using replace()

---
+++ remove_exclamation_marks.sql
@@ -0,0 +1,10 @@
+-- # Remove Exclamation Marks
+
+/*
+
+Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+*/
+
+SELECT s, REPLACE(s, '!', '') as res
+FROM removeexclamationmarks
