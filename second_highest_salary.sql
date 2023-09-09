1b3ac9a71a074fd34f4486b3a27a84c7295320e2
burd5 <austinburdette5@gmail.com>
2023-09-09 10:36:06 -0600

    completed challenge by using max and subquery. Looked at and learned about offset

---
+++ second_highest_salary.sql
@@ -0,0 +1,23 @@
+-- Second Highest Salary
+
+/*
+
+Write a solution to find the second highest salary from the Employee table. If there is no second highest salary, return null (return None in Pandas).
+
+*/
+
+-- My Solution
+
+SELECT MAX(salary) as SecondHighestSalary
+FROM employee
+WHERE salary <> (SELECT MAX(salary) FROM employee)
+
+-- Other Solutions
+
+SELECT
+    (SELECT DISTINCT Salary 
+    FROM Employee
+    ORDER BY Salary DESC
+    LIMIT 1 OFFSET 1) 
+AS SecondHighestSalary;
+
