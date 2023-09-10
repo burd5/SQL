049d1544a862626c6161fd0198b28e8b7bee5446
burd5 <austinburdette5@gmail.com>
2023-09-10 08:23:24 -0600

    looked at multiple solutions including ones that used variables, lead(), and self joins

---
+++ consecutive_numbers.sql
@@ -0,0 +1,65 @@
+-- Consecutive Numbers
+
+/*
+Find all numbers that appear at least three times consecutively.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+Example 1:
+
+Input: 
+Logs table:
++----+-----+
+| id | num |
++----+-----+
+| 1  | 1   |
+| 2  | 1   |
+| 3  | 1   |
+| 4  | 2   |
+| 5  | 1   |
+| 6  | 2   |
+| 7  | 2   |
++----+-----+
+Output: 
++-----------------+
+| ConsecutiveNums |
++-----------------+
+| 1               |
++-----------------+
+Explanation: 1 is the only number that appears consecutively for at least three times.
+
+*/
+
+-- establishes variables counter and prev to keep track of repeated numbers
+SELECT  DISTINCT(Num) AS ConsecutiveNums
+FROM (
+    SELECT
+    Num,
+    @counter := IF(@prev = Num, @counter + 1, 1) AS how_many_cnt_in_a_row,
+    @prev := Num
+    FROM Logs y, (SELECT @counter:=1, @prev:=NULL) vars
+) sq
+WHERE how_many_cnt_in_a_row >= 3
+
+-- uses Id + 1, Num and Id + 2, Num to see if these values are present in Logs. If so, there must be 3 in a row
+SELECT distinct Num as ConsecutiveNums
+FROM Logs
+WHERE (Id + 1, Num) in (select * from Logs) and (Id + 2, Num) in (select * from Logs)
+
+-- uses lead  with cte as (
+    select num,
+    lead(num,1) over() num1,
+    lead(num,2) over() num2
+    from logs
+
+)
+
+select distinct num ConsecutiveNums from cte where (num=num1) and (num=num2)
+
+-- self join 3 times
+
+SELECT DISTINCT a.Num AS ConsecutiveNums FROM Logs a 
+JOIN Logs b ON a.Id = B.id + 1 AND a.Num = b.Num
+JOIN Logs c ON a.Id = C.id + 2 AND a.Num = c.Num
