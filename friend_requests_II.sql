81672a61e8f7fb8129c2a0fb4e985448cf345087
burd5 <austinburdette5@gmail.com>
2023-11-18 06:45:52 -0700

    completed query using union all

--- friend_requests_II.sql
+++ friend_requests_II.sql
@@ -46,3 +46,10 @@ The person with id 3 is a friend of people 1, 2, and 4, so he has three friends 
 */
 
 -- My Solution
+
+with base as(select requester_id id from RequestAccepted
+union all
+select accepter_id id from RequestAccepted)
+
+
+select id, count(*) num  from base group by 1 order by 2 desc limit 1
