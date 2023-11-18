c7643bc794d076b4b6cc15484edb1887aacd327e
burd5 <austinburdette5@gmail.com>
2023-11-18 06:47:00 -0700

    completed query using union all

--- friend_requests_II.sql
+++ friend_requests_II.sql
@@ -53,3 +53,13 @@ select accepter_id id from RequestAccepted)
 
 
 select id, count(*) num  from base group by 1 order by 2 desc limit 1
+
+-- 
+
+select id, count(*) num from 
+(
+      (select requester_id id from request_accepted) 
+      union all 
+      (select accepter_id id from request_accepted)
+) tb 
+group by id order by num desc limit 1
