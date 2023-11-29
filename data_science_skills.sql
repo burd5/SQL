7990b9c4bf57adc174373ed5b6e3532421a92ed6
burd5 <austinburdette5@gmail.com>
2023-11-28 17:16:57 -0700

    compledted query using having clause

---
+++ data_science_skills.sql
@@ -0,0 +1,30 @@
+-- Data Science Skills
+
+/*
+
+Given a table of candidates and their skills, you're tasked with finding the candidates best suited for an open Data Science job. You want to find candidates who are proficient in Python, Tableau, and PostgreSQL.
+
+Write a query to list the candidates who possess all of the required skills for the job. Sort the output by candidate ID in ascending order.
+
+*/
+
+-- My Solution
+
+with skills_counts as (SELECT candidate_id, COUNT(candidate_id) as skills_count
+FROM candidates
+WHERE skill in ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY candidate_id)
+
+select candidate_id
+from skills_counts
+where skills_count = 3
+order by 1 asc
+
+-- better solution
+
+SELECT candidate_id
+FROM candidates
+WHERE skill IN ('Python', 'Tableau', 'PostgreSQL')
+GROUP BY candidate_id
+HAVING COUNT(skill) = 3
+ORDER BY candidate_id;
