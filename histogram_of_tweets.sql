dbdd691869b9609254ef0cd9a38b8dd76fc20141
burd5 <austinburdette5@gmail.com>
2023-11-29 16:14:06 -0700

    completed challenge using cte and group by clauses

---
+++ histogram_of_tweets.sql
@@ -0,0 +1,22 @@
+-- Histogram of Tweets
+
+/*
+
+Assume you're given a table Twitter tweet data, write a query to obtain a histogram of tweets posted per user in 2022. Output the tweet count per user as the bucket and the number of Twitter users who fall into that bucket.
+
+In other words, group the users by the number of tweets they posted in 2022 and count the number of users in each group.
+
+*/
+
+
+-- My Solution
+
+with tweet_counts as (
+  SELECT COUNT(tweet_id) as num_of_tweets
+  FROM tweets
+  WHERE date_part('year', tweet_date) = 2022
+  GROUP BY user_id)
+
+SELECT num_of_tweets, COUNT(num_of_tweets) as users_num
+FROM tweet_counts
+GROUP BY 1
