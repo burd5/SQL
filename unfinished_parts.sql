e019b6a5e21faf1c2bd029413e185cc47ed1a03a
burd5 <austinburdette5@gmail.com>
2023-12-01 17:27:19 -0700

    completed query using isnull statement

---
+++ unfinished_parts.sql
@@ -0,0 +1,19 @@
+-- Unfinished Parts
+
+/*
+
+https://datalemur.com/questions/tesla-unfinished-parts
+
+Tesla is investigating production bottlenecks and they need your help to extract the relevant data. Write a query to determine which parts have begun the assembly process but are not yet finished.
+
+*/
+
+-- My Solution
+
+SELECT part, assembly_step
+FROM parts_assembly
+WHERE finish_date IS NULL;
+
+
+-- Other Solutions
+
