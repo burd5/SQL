971178778ad5fcb5601e406946cd7e77222dfcba
burd5 <austinburdette5@gmail.com>
2023-12-06 08:42:03 -0700

    completed query using CTE and by finding count(job_count) > 1

---
+++ duplicate_job_listings.sql
@@ -0,0 +1,38 @@
+-- Duplicate Job Listings
+
+/*
+
+https://datalemur.com/questions/duplicate-job-listings
+
+*/
+
+-- My Solution
+
+WITH job_count_cte AS (
+  SELECT 
+    company_id, 
+    title, 
+    description, 
+    COUNT(job_id) AS job_count
+  FROM job_listings
+  GROUP BY company_id, title, description
+)
+
+SELECT COUNT(DISTINCT company_id) AS duplicate_companies
+FROM job_count_cte
+WHERE job_count > 1;
+
+-- Other Solutions
+
+SELECT COUNT(DISTINCT company_id) AS duplicate_companies
+FROM (
+  SELECT 
+    company_id, 
+    title, 
+    description, 
+    COUNT(job_id) AS job_count
+  FROM job_listings
+  GROUP BY company_id, title, description
+) AS job_count_cte
+WHERE job_count > 1;
+
