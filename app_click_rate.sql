7698fbfaee0992bc559c21e174b594ea09025723
burd5 <austinburdette5@gmail.com>
2023-12-13 08:23:54 -0700

    completed query using CTE and sum(case when) statements

---
+++ app_click_rate.sql
@@ -0,0 +1,54 @@
+-- App Click Through Rate
+
+/*
+
+https://datalemur.com/questions/click-through-rate
+
+*/
+
+-- My Solution
+
+with click_rate as (
+select app_id,
+       SUM(CASE WHEN event_type = 'click' THEN 1 ELSE 0 end) as clicks,
+       SUM(CASE WHEN event_type = 'impression' THEN 1 ELSE 0 end) as impressions
+from events
+where EXTRACT(YEAR from timestamp) = 2022
+group by 1
+)
+
+select app_id, ROUND(100.0 * clicks/impressions, 2)
+from click_rate
+
+-- Solutions without CTEs
+
+SELECT
+  app_id,
+  ROUND(100.0 *
+    SUM(CASE WHEN event_type = 'click' THEN 1 ELSE 0 END) /
+    SUM(CASE WHEN event_type = 'impression' THEN 1 ELSE 0 END), 2)  AS ctr_rate
+FROM events
+WHERE timestamp >= '2022-01-01' 
+  AND timestamp < '2023-01-01'
+GROUP BY app_id;
+
+
+SELECT
+  app_id,
+  ROUND(100.0 *
+    COUNT(CASE WHEN event_type = 'click' THEN 1 ELSE NULL END) /
+    COUNT(CASE WHEN event_type = 'impression' THEN 1 ELSE NULL END), 2)  AS ctr_rate
+FROM events
+WHERE timestamp >= '2022-01-01' 
+  AND timestamp < '2023-01-01'
+GROUP BY app_id;
+
+SELECT
+  app_id,
+  ROUND(100.0 *
+    SUM(1) FILTER (WHERE event_type = 'click') /
+    SUM(1) FILTER (WHERE event_type = 'impression'), 2) AS ctr_app
+FROM events
+WHERE timestamp >= '2022-01-01' 
+  AND timestamp < '2023-01-01'
+GROUP BY app_id;
