d1883375982b0f5755700e6f75268c6474182ae4
burd5 <austinburdette5@gmail.com>
2023-12-15 08:41:16 -0700

    completed query using max/min and group by

---
+++ card_issued_difference.sql
@@ -0,0 +1,14 @@
+-- Card Issued Difference
+
+/*
+
+https://datalemur.com/questions/cards-issued-difference
+
+*/
+
+-- My Solution
+
+SELECT card_name, MAX(issued_amount) - MIN(issued_amount) as difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY 2 desc;
