547f6845889c977e82aa125f0045c01ee97cf88e
burd5 <austinburdette5@gmail.com>
2023-12-23 11:40:12 -0500

    completed query using avg() over () rows between __ preceding and current row

---
+++ tweets_rolling_averages.sql
@@ -0,0 +1,29 @@
+-- Tweets Rolling Averages
+
+/*
+
+https://datalemur.com/questions/rolling-average-tweets
+
+*/
+
+-- My Solution
+
+SELECT user_id, tweet_date,
+       ROUND(AVG(tweet_count) OVER(
+       PARTITION BY user_id 
+       ORDER BY tweet_date 
+       ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
+       , 2) as rolling_avg_3d
+FROM tweets;
+
+-- Provided Solutions
+
+SELECT    
+  user_id,    
+  tweet_date,   
+  ROUND(AVG(tweet_count) OVER (
+    PARTITION BY user_id     
+    ORDER BY tweet_date     
+    ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
+  ,2) AS rolling_avg_3d
+FROM tweets;
