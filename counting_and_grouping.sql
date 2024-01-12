88ade88bb4111b6ea893da50eacf574cad83c0b4
burd5 <austinburdette5@gmail.com>
2024-01-12 16:39:37 -0700

    completed using count, group by, and order by

---
+++ counting_and_grouping.sql
@@ -0,0 +1,27 @@
+-- Counting and Grouping
+
+/*
+
+Given a demographics table in the following format:
+
+** demographics table schema **
+
+id
+name
+birthday
+race
+you need to return a table that shows a count of each race represented, ordered by the count in descending order as:
+
+** output table schema **
+
+race
+count
+
+*/
+
+-- Solution
+
+select race, count(*) 
+from demographics 
+group by race 
+order by 2 desc
