21ddd66881494ffac2a9148af324c12925f5ef08
burd5 <austinburdette5@gmail.com>
2024-01-16 15:33:57 -0700

    redid query. Ended up using subquery solution that I had looked at before

--- managers_with_least_5_direct_reports.sql
+++ managers_with_least_5_direct_reports.sql
@@ -63,3 +63,9 @@ where id in
 (select managerId from Employee
 group by managerId
 having count(managerId)>=5) 
+
+-- Redid Solution
+
+select name
+from employee
+where id in (select managerId from employee group by managerId having count(managerId) >= 5)
