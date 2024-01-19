ca23a916db0908f1b301eaf4f7f707ea9fc0e87e
burd5 <austinburdette5@gmail.com>
2024-01-19 16:22:09 -0700

    completed query using postgres, to_char, sum, and case when statements

--- monthly_transactions_I.sql
+++ monthly_transactions_I.sql
@@ -70,3 +70,14 @@ AS month,
        SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions
 GROUP BY month, country;
+
+-- PostgresQL Attempt
+
+select to_char(trans_date, 'YYYY-MM') as month,
+       country,
+       count(*) as trans_count,
+       SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) as approved_count,
+       SUM(amount) as trans_total_amount,
+       SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) as approved_total_amount
+from transactions
+group by 1,2
