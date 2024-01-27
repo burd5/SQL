3572e6358b2523586559790fc784325dd2043497
burd5 <austinburdette5@gmail.com>
2024-01-27 10:38:46 -0700

    completed query same as first time using join

--- product_sales_analysis_I.sql
+++ product_sales_analysis_I.sql
@@ -73,4 +73,9 @@ Output:
 SELECT p.product_name, s.year, s.price
 FROM sales s JOIN product p USING(product_id)
 
+-- 2nd attempt
+
+select product_name, year, price
+from sales join product using(product_id)
+
 
