1144ed02b9b3b68c7b3b2de4683b653b8c5779b6
burd5 <austinburdette5@gmail.com>
2024-02-09 20:24:19 -0700

    struggled to complete query. Ended up peaking at CTE solution that counted matches between rows and then queried CTE count

--- duplicate_job_listings.sql
+++ duplicate_job_listings.sql
@@ -36,3 +36,14 @@ FROM (
 ) AS job_count_cte
 WHERE job_count > 1;
 
+-- 2nd attempt
+
+with duplicate_jobs as (
+  select company_id, title, description, count(job_id) as job_count
+  from job_listings
+  group by company_id, title, description
+)
+
+select count(distinct company_id) as duplicate_companies
+from duplicate_jobs
+where job_count > 1
