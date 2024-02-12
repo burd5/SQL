d9732cdbe9879480ecca26bc7f01629c1481811a
burd5 <austinburdette5@gmail.com>
2024-02-12 13:45:05 -0700

    completed query using subquery which was way extra. Could've done same thing just using where clause

--- second_day_confirmation.sql
+++ second_day_confirmation.sql
@@ -22,3 +22,15 @@ WHERE texts.action_date = emails.signup_date + INTERVAL '1 day'
   AND texts.signup_action = 'Confirmed';
 
 
+-- 2nd attempt
+-- way too extra, subquery not needed
+
+select distinct user_id
+from emails e  
+join texts t  
+on e.email_id = t.email_id
+where (user_id, signup_date + INTERVAL '1 day') in (
+  select user_id, action_date
+  from texts join emails on texts.email_id = emails.email_id
+  where signup_action = 'Confirmed'
+)
