803264add89d14c44f6b765533fcd0aeca43c59f
burd5 <austinburdette5@gmail.com>
2024-02-24 17:58:52 -0700

    completed query in same manner as last time

--- card_issued_difference.sql
+++ card_issued_difference.sql
@@ -12,3 +12,12 @@ SELECT card_name, MAX(issued_amount) - MIN(issued_amount) as difference
 FROM monthly_cards_issued
 GROUP BY card_name
 ORDER BY 2 desc;
+
+-- 2nd attempt
+
+SELECT
+  card_name,
+  MAX(issued_amount) - MIN(issued_amount) as difference
+FROM monthly_cards_issued
+GROUP BY card_name
+ORDER BY 2 DESC
