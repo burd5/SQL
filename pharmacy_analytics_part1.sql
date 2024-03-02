559325fe4246a28ec7e0b22a2b27b5d7978d46f3
burd5 <austinburdette5@gmail.com>
2024-03-01 17:09:01 -0700

    completed query using sum and group by which was not neccessary. Looked at simpler solution

--- pharmacy_analytics_part1.sql
+++ pharmacy_analytics_part1.sql
@@ -15,3 +15,18 @@ LIMIT 3;
 
 
 
+-- 2nd Attempt
+
+-- top 3 most profitable drugs
+-- how much profit
+-- profit DESC
+
+-- UNNECCESSARY TO GROUP BY HERE
+
+SELECT
+  drug,
+  SUM(total_sales - cogs) as total_profit -- don't need sum either
+FROM pharmacy_sales
+-- GROUP BY drug
+ORDER BY total_profit DESC
+LIMIT 3;
