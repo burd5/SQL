3c5629f691643e739a0fa92d61f0369209fa4151
burd5 <austinburdette5@gmail.com>
2024-03-25 11:32:34 -0600

    struggled to remember rows between 2 preceding and current row element of solution. Need to restudy

--- tweets_rolling_averages.sql
+++ tweets_rolling_averages.sql
@@ -27,3 +27,19 @@ SELECT
     ROWS BETWEEN 2 PRECEDING AND CURRENT ROW)
   ,2) AS rolling_avg_3d
 FROM tweets;
+
+-- 2nd attempt
+
+-- rolling 3 day tweet average per user
+-- user_id, tweet_date, tweet_count
+
+-- SUM(tweets) between two_days_before and current_day, group by user
+
+select
+  user_id,
+  tweet_date,
+  ROUND(AVG(tweet_count) OVER(
+    partition by user_id 
+    order by tweet_date
+    ROWS BETWEEN 2 PRECEDING AND CURRENT ROW), 2) as rolling_avg_3d
+from tweets
