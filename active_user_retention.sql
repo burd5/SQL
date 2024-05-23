093dc51b69057c41bba31abee5503ad9201b11bb
burd5 <austinburdette5@gmail.com>
2024-05-23 17:00:16 -0600

    added provided solution for active user retention

--- active_user_retention.sql
+++ active_user_retention.sql
@@ -20,3 +20,20 @@ select
   COUNT(user_id) as monthly_active_users
 from active_month_counts
 where june_count > 0 and july_count > 0
+
+-- Provided Solution
+
+SELECT 
+  EXTRACT(MONTH FROM curr_month.event_date) AS mth, 
+  COUNT(DISTINCT curr_month.user_id) AS monthly_active_users 
+FROM user_actions AS curr_month
+WHERE EXISTS (
+  SELECT last_month.user_id 
+  FROM user_actions AS last_month
+  WHERE last_month.user_id = curr_month.user_id
+    AND EXTRACT(MONTH FROM last_month.event_date) =
+    EXTRACT(MONTH FROM curr_month.event_date - interval '1 month')
+)
+  AND EXTRACT(MONTH FROM curr_month.event_date) = 7
+  AND EXTRACT(YEAR FROM curr_month.event_date) = 2022
+GROUP BY EXTRACT(MONTH FROM curr_month.event_date);
