7a1f7ed680bc0a3b9547144d45a61c83ae554f4f
burd5 <austinburdette5@gmail.com>
2024-06-02 12:52:51 -0600

    completed challenge using EXTRACT and LAG

---
+++ repeated_payments.sql
@@ -0,0 +1,16 @@
+-- Repeated Payments
+
+-- https://datalemur.com/questions/repeated-payments
+
+-- FINAL SOLUTION
+
+with minutes_diff as (
+  SELECT *,
+         EXTRACT(EPOCH FROM transaction_timestamp - LAG(transaction_timestamp) 
+         OVER (partition by merchant_id, credit_card_id, amount order by transaction_timestamp)) / 60 as minute_difference
+  FROM transactions
+)
+
+select count(*) as payment_count
+from minutes_diff
+where minute_difference <= 10 
